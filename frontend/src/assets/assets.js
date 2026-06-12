9c1f9f03774f74ed8d5999b8f9e6953f9140bae9
Sain Rai <sahinkulung@gmail.com>
2026-06-13 01:53:16 +0545

    added assets file with image and some other changes

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,19 @@
+import a from './a.png'
+import b from './b.png'
+import c from './c.png'
+import d from './d.png'
+import e from './e.png'
+import f from './f.png'
+import g from './g.png'
+import h from './h.png'
+import h from './h.png'
+import i from './i.png'
+import j from './j.png'
+import o from './o.png'
+import m from './m.png'
+import style_street_logo from './style_street_logo.png'
+
+export const assets ={
+    logo,
+    her
+}
