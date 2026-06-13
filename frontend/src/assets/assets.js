eb9efd7693d08b82a60d99d6d1778dc57c85b52e
Sain Rai <sahinkulung@gmail.com>
2026-06-14 00:32:00 +0545

    Pages Updated

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,49 +1,47 @@
-import a from './a.png'
-import b from './b.png'
-import c from './c.png'
-import d from './d.png'
-import icon from './icon.png'
-import icon1 from './icon1.png'
-import icon2 from './icon2.png'
-import icon3 from './icon3.png'
-import icon4 from './icon4.png'
-import icon5 from './icon5.png'
+import a from './a.jpg'
+import b from './b.jpg'
+import c from './c.jpg'
+import cart from './cart.png'
+import d from './d.jpg'
+import search from './search.png'
+import user from './user.png'
+
 import style_street_logo from './style_street_logo.png'
 
 export const assets = {
-  icon,
-  icon1,
-  icon2,
-  icon3,
-  icon4,
-  icon5,
-  style_street_logo
+  user,
+  search,
+  cart,
+
+  style_street_logo,
 }
 
 export const products = [
   {
     _id: "aaaaa",
     name: "Street wear 1",
-    description: "A lightweight, usually knitted, puullovershirt, close-fitting and easy wear",
+    description:
+      "A lightweight, usually knitted, puullovershirt, close-fitting and easy wear",
     price: 100,
     image: [a, b, c, d],
     category: "women",
     subCategory: "full outfit",
     sizes: ["s", "m", "l"],
     date: 1716634345448,
-    bestseller: true
+    bestseller: true,
   },
   {
-        _id: "aaaab",
+    _id: "aaaab",
     name: "Street wear 2",
-    description: "A lightweight, usually knitted, puullovershirt, close-fitting and easy wear",
+    description:
+      "A lightweight, usually knitted, puullovershirt, close-fitting and easy wear",
     price: 100,
     image: [a, b, c, d],
     category: "women",
     subCategory: "full outfit",
     sizes: ["s", "m", "l"],
     date: 1716634345448,
-    bestseller: true
+    bestseller: true,
   },
-];
+]
 
