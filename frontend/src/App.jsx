110d493e150616dd59683087cd155fbc9d43ed7b
Sain Rai <sahinkulung@gmail.com>
2026-06-13 16:06:26 +0545

    Route setup and Pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3x1 font-bold text-red-500"> Hello Style Street!</h1>
+    <div className ='px-4 sm:px-[5vw] md:px-[7vw] lg:px -[9vw]'>
+    <Routes>
+      < Route path = '/' element={<Home/>}/ >
+      <Route path = '/collection'element={<Collection/>}/>
+      <Route path = '/about' element = {<About/>}/>
+      <Route path = '/contact' element = {<Contact/>}/>
+      <Route path = '/login' element = {<login/>}/>
+      <Route path = '/product:productID' element = {<Product/>}/>
+      <Route path = '/cart' element = {<Cart/>}/>
+      <Route path = '/orders' element = {<Orders/>}/>
+      <Route path = '/PlaceOrder' element = {<PlaceOrder/>}/>
+    </Routes>
     </div>
   )
 }
