0a74627ae544b753d11fcb912413aae6bc6625c4
Sain Rai <sahinkulung@gmail.com>
2026-06-14 23:53:47 +0545

    new changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,11 +8,41 @@ const NavBar = () => {
         <img src = {assets.style_street_logo} className ='w-36' alt=""/>
         <ul className ='hidden sm:flex gap-5 text-sm text-gray-700'>
           <NavLink to ='/' className = 'flex flex-col items-center gap-1'>
-            <p>Home</p>
+            <p>HOME</p>
+            <hr className = 'w-2/4 border-none h-[1.5px] bg-gray-700'/>
+          </NavLink>
+           <NavLink to ='/collection' className = 'flex flex-col items-center gap-1'>
+            <p>COLLECTION</p>
+            <hr className = 'w-2/4 border-none h-[1.5px] bg-gray-700'/>
+          </NavLink>
+           <NavLink to ='/about' className = 'flex flex-col items-center gap-1'>
+            <p>ABOUT</p>
+            <hr className = 'w-2/4 border-none h-[1.5px] bg-gray-700'/>
+          </NavLink>
+           <NavLink to ='/contact' className = 'flex flex-col items-center gap-1'>
+            <p>CONTACT</p>
             <hr className = 'w-2/4 border-none h-[1.5px] bg-gray-700'/>
           </NavLink>
 
+
         </ul>
+        <div className='flex items-center gap-6'>
+          <img src = { assets.search} className='w-5 cursor-pointer' alt=""/>
+         
+          <div className='group relative'>
+            <img className='w-5 cursor-pointer'src={assets.user} alt =""/>
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className=' flex flex-col gap-2 w-26 py-3 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logout</p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+          </div>
+
+        </div>
     </div>
   )
 }
