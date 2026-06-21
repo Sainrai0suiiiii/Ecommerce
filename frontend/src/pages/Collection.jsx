0243c6e2f2835a603f545d5cc8a82585b9f85222
Sain Rai <sahinkulung@gmail.com>
2026-06-22 01:34:41 +0545

    Collection

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -4,10 +4,21 @@ import { ShopContext } from '../context/ShopContext'
 const Collection = () => {
 
   const { products }= useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
-      {/*  */}
+      {/* filer options */}
+
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex item-center cursor-pointer gap-2'>FILTERS</p>
+        {/*  Category filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6`}>
+
+
+        </div>
+      </div>
     </div>
   )
 }
