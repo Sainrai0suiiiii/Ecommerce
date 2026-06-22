9ac4b34a704b27988ad1f99fe563ac050945047c
Sain Rai <sahinkulung@gmail.com>
2026-06-23 01:16:14 +0545

    map products

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,11 +1,18 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
   const {products} = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filerProducts,setFilterProducts]  = useState([]);
+
+  useEffect(() =>{
+    setFilterProducts(products);
+  },[])
 
   return (
     
@@ -52,9 +59,25 @@ const Collection = () => {
 
       {/* right side */}
 
-      
-
-
+      <div className=' flex-1'>
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'}/>
+          {/* Product sort */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value= "relavent">Sort by: Relavent</option>
+            <option value = "low-high">Sort by: Low to High</option>
+            <option value = "high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+        {/* Map Products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filerProducts.map((item,index)=>(
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image}/>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
