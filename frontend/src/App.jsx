a02f672b17470fb9240a4ea5dbadb928dc83bdfa
Sain Rai <sahinkulung@gmail.com>
2026-06-26 23:56:57 +0545

    Connected it from the navbar “My Profile” in  action

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import PlaceOrder from './pages/PlaceOrder'
 import Product from './pages/Product'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/login" element={<LoginModule.default />} />
 
         <Route path="/product/:productID" element={<Product />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/PlaceOrder" element={<PlaceOrder />} />
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -58,9 +58,9 @@ const NavBar = () => {
           <img src={assets.user} className="w-5 cursor-pointer" alt="" />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-26 py-3 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+              <Link to="/profile" className="cursor-pointer px-3 py-1 hover:text-black">My Profile</Link>
+              <Link to="/orders" className="cursor-pointer px-3 py-1 hover:text-black">Orders</Link>
+              <p className="cursor-pointer px-3 py-1 hover:text-black">Logout</p>
             </div>
           </div>
         </div>
